6b3f6fc0fdcae94b9cd135991c7ffd8cef4ea7c9
André Filipe <eutech.backup@gmail.com>
2022-05-01 13:28:52 -0300

    :truck: :dizzy: Script removed and CSS animation added

--- src/scripts/reveal.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', () => {
-    let cell = document.querySelector('.cell');
-    let desc = document.querySelector('.description');
-    cell.classList.add('cell--reveal');
-    desc.classList.add('description--reveal');
-});
